0606ba6b76bb5b5877772714e045280cf8b46a62
wallace <exnesswallace@gmail.com>
2024-07-07 02:53:18 +0200

    added docstring and fixed pycodestyle

--- app/models/course.py
+++ app/models/course.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
-
+"""
+    imports necessary for the program
+"""
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text
 from database import Base
 
 
 class Course(Base):
+    """
+        Course model for representing courses in the database.
+
+        Attributes:
+            id (int): The primary key identifier for the course.
+            name (str): The name of the course.
+            price (int): The price of the course.
+            duration (str): The duration of the course.
+            description (str): The description of the course.
+            certification (bool): Whether the course provides certification.
+    """
     __tablename__ = "courses"
 
     id = Column(Integer, primary_key=True, nullable=False)
@@ -13,4 +26,14 @@ class Course(Base):
     price = Column(Integer, nullable=True)
     duration = Column(String(length=25), nullable=False)
     description = Column(Text, nullable=True)
-    Certifaction = Column(Boolean, nullable=False)
+    certifaction = Column(Boolean, nullable=False)
+
+    def __init__(self, id, name, price, description, certification):
+        """
+            Initialize a new Course instance
+        """
+        self.id = id
+        self.name = name
+        self.price = price
+        self.description = description
+        self.certification = certification
