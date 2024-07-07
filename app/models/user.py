3e2fbddeba953e71ea740cdad7495a2b735b746c
wallace <exnesswallace@gmail.com>
2024-07-07 02:47:46 +0200

    added docstring and fixed pycodestyle

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    imports necessary for the program
+"""
 from sqlalchemy import Column, String, Integer, Boolean, DateTime
 from database import Base
 from datetime import datetime
 
+
 class User(Base):
+    """
+        User model for representing users in the database.
+
+        Attributes:
+            id (str): The primary key identifier for the user.
+            name (str): The first name of the user.
+            surname (str): The surname of the user.
+            instructor (bool): Whether the user is an instructor.
+            nationality (str): The nationality of the user.
+            email (str): The email address of the user.
+            password (str): The password of the user.
+            date_created (datetime): The date and time when the
+                                     user was created.
+    """
     __tablename__ = 'users'
 
     id = Column(String, primary_key=True)
@@ -14,7 +34,11 @@ class User(Base):
     password = Column(String, nullable=False)
     date_created = Column(DateTime, default=datetime.utcnow)
 
-    def __init__(self, id, name, surname, instructor, nationality, email, password):
+    def __init__(self, id, name, surname, instructor, nationality,
+                 email, password):
+        """
+            Initialize a new User instance.
+        """
         self.id = id
         self.name = name
         self.surname = surname
@@ -22,4 +46,3 @@ class User(Base):
         self.nationality = nationality
         self.email = email
         self.password = password
-
