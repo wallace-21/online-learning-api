edb34b4c95471eaf2dd3138cf39c0a8e7f6aafd2
wallace <exnesswallace@gmail.com>
2024-07-07 02:46:05 +0200

    fixed pycodestyle

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """
     imports neseccery for the program
@@ -9,15 +9,20 @@ from database import get_db
 from models.user import User
 
 
-#Initialize the request parser and add expected arguments for user creation
+# Initialize the request parser and add expected arguments for user creation
 user_post_args = reqparse.RequestParser()
-user_post_args.add_argument("name", type=str, help="Name shouldn't be empty", required=True)
-user_post_args.add_argument("surname", type=str, help="Surname shouldn't be empty", required=True)
-user_post_args.add_argument("nationality", type=str, help="Nationality shouldn't be empty", required=True)
-user_post_args.add_argument("email", type=str, help="Email shouldn't be empty", required=True)
+user_post_args.add_argument("name", type=str,
+                            help="Name shouldn't be empty", required=True)
+user_post_args.add_argument("surname", type=str,
+                            help="Surname shouldn't be empty", required=True)
+user_post_args.add_argument("nationality", type=str,
+                            help="Nationality shouldn't be empty",
+                            required=True)
+user_post_args.add_argument("email", type=str,
+                            help="Email shouldn't be empty", required=True)
 user_post_args.add_argument("password", help="Set a password", required=True)
 
-#Define the resource fields for marshalling response data
+# Define the resource fields for marshalling response data
 resource_fields = {
     "name": fields.String,
     "surname": fields.String,
@@ -27,7 +32,7 @@ resource_fields = {
     "date_created": fields.DateTime
 }
 
-#Define the full resource fields, including more information
+# Define the full resource fields, including more information
 full_resource_fields = {
     "id": fields.String,
     "name": fields.String,
@@ -39,28 +44,37 @@ full_resource_fields = {
     "date_created": fields.DateTime
 }
 
+
 class UserResource(Resource):
     """
-        Resource for handling individual user operations, such as creating and retrieving a user.
+        Resource for handling individual user operations,
+        such as creating and retrieving a user.
     """
     @marshal_with(full_resource_fields)
     def post(self):
         """
             Handle POST requests to create a new user.
 
-            Parses the request arguments, creates a new user, and saves it to the database.
+            Parses the request arguments, creates a new user,
+            and saves it to the database.
 
             Returns:
                 The created user object and a 201 status code.
         """
         args = user_post_args.parse_args()
-        user = User(id=str(uuid4()), name=args["name"], surname=args["surname"],
-                    instructor=args.get("instructor", False), nationality=args["nationality"],
-                    email=args["email"], password=args["password"])
+        user = User(id=str(uuid4()),
+                    name=args["name"],
+                    surname=args["surname"],
+                    instructor=args.get("instructor", False),
+                    nationality=args["nationality"],
+                    email=args["email"],
+                    password=args["password"])
+
         with get_db() as db:
             db.add(user)
             db.commit()
-            #Refresh the user instance to ensure all fields are properly populated
+            # Refresh the user instance to ensure all
+            # fields are properly populated
             db.refresh(user)
         return user, 201
 
@@ -73,13 +87,15 @@ class UserResource(Resource):
                 user_id (str): The ID of the user to retrieve.
 
             Returns:
-                The user object if found, or a 404 error if the user does not exist.
+                The user object if found, or a 404 error
+                if the user does not exist.
         """
         with get_db() as db:
             user = db.query(User).filter_by(id=user_id).first()
             if not user:
                 abort(404, massege="user not found")
             return user
+
 
 class UsersResource(Resource):
     """
