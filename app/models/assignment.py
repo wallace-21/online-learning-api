72d869ec664de5ea2f0daa92731068ac25daf4a1
wallace <exnesswallace@gmail.com>
2024-07-07 03:03:44 +0200

    added docstring and fixed pycodestyle

--- app/models/assignment.py
+++ app/models/assignment.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-
+"""
+    imports needed to run the program
+"""
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 from database import Base
 from datetime import datetime
 
 
 class Assignment(Base):
+    """
+        Assignment model for representing assignments in the database.
+
+        Attributes:
+            id (int): The primary key identifier for the assignment.
+            name (str): The name of the assignment.
+            released_date (datetime): The release date of the assignment.
+            due_date (datetime): The due date of the assignment.
+            description (str): The description of the assignment.
+    """
     __tablename__ = "assignment"
 
     id = Column(Integer, primary_key=True, nullable=False)
@@ -14,3 +26,13 @@ class Assignment(Base):
     released_date = Column(DateTime, default=datetime.now)
     due_date = Column(DateTime, nullable=False)
     description = Column(Text, nullable=False)
+
+    def __init__(self, id, name, released_date, due_date, description):
+        """
+            Initialize a new Assignment instance.
+        """
+        self.id = id
+        self.name = name
+        self.released_date = released_date
+        self.due_date = due_date
+        self.description. description
