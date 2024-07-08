a14aa704e74e7fef0b8e77b4a90abeb61bb94f69
wallace <exnesswallace@gmail.com>
2024-07-08 21:57:49 +0200

    added docstring

--- app/routes/course.py
+++ app/routes/course.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    imports necessery for the program to run
+"""
+
 from flask_restful import Api, Resource, reqparse, marshal_with, fields, abort
 from database import get_db
 from uuid import uuid4
 from models.course import Course
 
-
+# Parser for POST request arguments (creates)
 course_post_args = reqparse.RequestParser()
 course_post_args.add_argument("name", type=str, help="Enter name of the course", required=True)
 course_post_args.add_argument("price", type=int, help="How much much does the course cost?", default=0)
@@ -11,6 +17,7 @@ course_post_args.add_argument("duration", type=str, help="How long does the the 
 course_post_args.add_argument("description", type=str, help="description of the course")
 course_post_args.add_argument("certification", type=bool, help="Does the course provide certification", default=False)
 
+# Parser for PUT request arguments (updates)
 course_update_args = reqparse.RequestParser()
 course_update_args.add_argument("name", type=str, help="Enter name of the course")
 course_update_args.add_argument("price", type=int, help="How much much does the course cost?",)
@@ -18,6 +25,7 @@ course_update_args.add_argument("duration", type=str, help="How long does the th
 course_update_args.add_argument("description", type=str, help="description of the course")
 course_update_args.add_argument("certification", type=bool, help="Does the course provide certification")
 
+# Fields for marshalling the Course model
 resource_fields = {
         "id": fields.String,
         "name": fields.String,
@@ -27,11 +35,22 @@ resource_fields = {
         "description": fields.String
 }
 
-
 class CourseResource(Resource):
-
+    """
+        Resource for handling individual course operations,
+        such as creating and retrieving courses.
+    """
     @marshal_with(resource_fields)
     def post(self):
+         """
+            Handle POST requests to create a new course.
+
+            Parses the request arguments, creates a new course,
+            and saves it to the database.
+
+            Returns:
+                The created course object and a 201 status code.
+        """
         args = course_post_args.parse_args()
         course = Course(id=str(uuid4()),
                         name=args["name"],
@@ -48,6 +67,16 @@ class CourseResource(Resource):
 
     @marshal_with(resource_fields)
     def get(self, course_name):
+        """
+            Handle GET requests to retrieve a user by ID.
+
+            Args:
+                course_name (str): The name of the course to retrieve.
+
+            Returns:
+                The course object if found, or a 404 error
+                if the course does not exist.
+        """
         with get_db() as db:
             course = db.query(Course).filter_by(name=course_name).first()
             if not course:
@@ -57,6 +86,15 @@ class CourseResource(Resource):
 
     @marshal_with(resource_fields)
     def put(self, course_name):
+        """
+            Update an existing user's details.
+
+            Args:
+                course_name (str): The name of the course to update.
+
+            Returns:
+                The updated course object and HTTP status code 201.
+            """
         args = course_update_args.parse_args()
 
         with get_db() as db:
@@ -79,6 +117,17 @@ class CourseResource(Resource):
             return course, 201
 
     def delete(self, course_name):
+        """ Delete an existing course.
+
+            Args:
+                course_name (str): The ID of the user to delete.
+
+            Raises:
+                404: If the course is not found.
+
+            Returns:
+                dict: A message confirming the course has been deleted.
+        """
         with get_db() as db:
             course = db.query(Course).filter_by(id=course_name).first()
             if not course:
@@ -88,9 +137,20 @@ class CourseResource(Resource):
             db.commit()
 
             return {"message": "Course deleted"}
+
 class CoursesResource(Resource):
+    """
+        Resource for handling operations to retrieving all users.
+    """
+
     @marshal_with(resource_fields)
     def get(self):
+        """
+            Handle GET requests to retrieve all courses.
+
+            Returns:
+                A list of all course objects.
+        """
         with get_db() as db:
             courses = db.query(Course).all()
             return courses, 201
