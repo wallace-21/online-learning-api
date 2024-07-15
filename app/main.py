1725dd78209060eba7bc17bfcb40df3064e9af35
wallace <exnesswallace@gmail.com>
2024-07-15 21:59:38 +0200

    added endpoints for quiz

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from database import Base, engine, get_db
 from models.user import User
 from routes.user import UserResource, UsersResource
 from routes.course import CourseResource, CoursesResource
-from routes.quiz import QuizResource, QuizzesResource
+from routes.quiz import QuizResource, QuizzesResource, CourseQuizzesResource
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
@@ -20,10 +20,11 @@ Base.metadata.create_all(bind=engine)
 """
 api.add_resource(UserResource, "/user", "/user/<string:user_id>")
 api.add_resource(UsersResource, "/users")
-api.add_resource(CourseResource, "/course", "/course/<string:course_name>")
+api.add_resource(CourseResource, "/course", "/course/<int:course_id>")
 api.add_resource(CoursesResource, "/courses")
 api.add_resource(QuizResource, "/quiz", "/quiz/<int:quiz_id>")
 api.add_resource(QuizzesResource, "/quizzes")
+api.add_resource(CourseQuizzesResource, "/course/<int:course_id>/quizzes")
 
 if __name__ == "__main__":
     app.run(debug=True)
